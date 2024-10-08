d3693eb0b402d274b3c4bebe2d769e6d06b7ee4c
Wil <wil512on@gmail.com>
2024-10-08 20:04:58 +0000

    cambios wil

--- dumpShipmentExportUPS.js
+++ dumpShipmentExportUPS.js
@@ -7,53 +7,54 @@ const from = {
   user: 'sa',
   password: 'Y259QcT',
   database: 'UPS_Control',
-  requestTimeout: 180000,
+  requestTimeout: 3600000,
   options: {
     trustServerCertificate: true,
     encrypt: false
   },
 }
-const to = 'mongodb://dev2/ups-cr?replicaSet=gpaxio'
+const to = 'mongodb://tenorio1,tenorio2/ups-cr?replicaSet=gpaxio'
 //const to = 'mongodb://gpax2,gpax3/gpax?replicaSet=gpax'
 const collection = 'shipmentExport'
 const collection2 = 'idMigration'
 const query =
   `SELECT
-            es.[FECHA_CARGA] as importDate,
+    es.[FECHA_CARGA] as importDate,
     es.[ORIGIN_COUNTRY] as originCountry,
     es.[ORIGIN_PORT] as originPort,
-    es.[DESTINATION_COUNTRY]as destinationCountry,
-    es.[DESTINATION_PORT]as destinationPort ,
-    es.[IMPORT_DATE] as deliveryDate ,
+    es.[DESTINATION_COUNTRY] as destinationCountry,
+    es.[DESTINATION_PORT] as destinationPort,
+    es.[IMPORT_DATE] as deliveryDate,
     es.[MAWB_NUMBER] as mawbNumber,
     es.[SHIPMENT_TYPE] as shipmentType,
-    es.[SHIPMENT_NUMBER] ,
-    es.[RECORD_TYPE] as recordType  ,
+    es.[SHIPMENT_NUMBER],
+    es.[RECORD_TYPE] as recordType,
     es.[SHIPMENT_ID] as _id,
     es.[BILLING_TYPE_SHIPMENT] as billingType,
-    es.[NUMBER_PACKAGE_IN_SHIPMENT]as numberPackages,
+    es.[NUMBER_PACKAGE_IN_SHIPMENT] as numberPackages,
     es.[SHIPMENT_WEIGHT] as shipmentWeight,
     es.[WEIGHT_UNIT] as shipmentWeightUnit,
-    es.[COUNTRY_ORIGIN_GOODS]as countryOriginGoods,
-    es.[DECLARED_VALUE] as declaredValue ,
-    es.[INVOICE_TOTAL] as invoiceTotal ,
+    es.[COUNTRY_ORIGIN_GOODS] as countryOriginGoods,
+    es.[DECLARED_VALUE] as declaredValue,
+    es.[INVOICE_TOTAL] as invoiceTotal,
     es.[CURRENCY_CODE_INVOICE_TOTAL] as invoiceCurrencyCode,
-    es.[THIRD_PARTY_INDICATOR_CODE] as thirdPartyIndicatorCode ,
+    es.[THIRD_PARTY_INDICATOR_CODE] as thirdPartyIndicatorCode,
     es.[SATURDAY_DELIVERY_FLAGS] as saturdayDeliveryFlag,
     es.[SPECIAL_INSTRUCTION_FLAGS] as specialInstructionFlag,
-    es.[INPUT_DATE] as inputDate ,
-    es.[SERVICE_LEVEL] as serverLevel,
+    es.[INPUT_DATE] as inputDate,
+    es.[SERVICE_LEVEL] as serviceLevel,
     es.[FREIGHT_COLLECT_FLAGS] as freightCollectFlag,
     es.[EARLY_AM_INDICATOR] as earlyAmIndicator,
     es.[OVER_31_8_KGS70_LBS_FLAGS] as overWeight,
     es.[DATE_SHIPMENT] as dateShipped,
     es.[BILLING_TERM_FIELD] as billingTermField,
     es.[DIMENSIONAL_WEIGHT] as dimensionalWeight,
-    es.[DIMENSIONAL_WIEGHT_UNIT] as dimensionalWeightUnit ,
+    es.[DIMENSIONAL_WIEGHT_UNIT] as dimensionalWeightUnit,
     es.[CONSECUTIVO] as consecutivo,
     es.[Peso_Kilos],
     es.[Peso_Libras],
 
+    -- Subconsulta para obtener las facturas
     (SELECT
         esi.[INVOICE_UNIT_QUANTITY] AS unitQuantity,
         esi.[INVOICE_CODE] AS code,
@@ -62,17 +63,21 @@ const query =
         esi.[INVOICE_CURRENCY_CODE] AS currencyCode,
         esi.[INVOICE_NUMBER] AS number,
         esi.[INVOICE_PAIS] AS countryOrigin
+     FROM [dbo].[EXPORT_SHIPMENT_INVOICE] esi
+     WHERE esi.[CONSECUTIVO_SHIPMENT] = es.[CONSECUTIVO]
      FOR JSON PATH) AS invoice,
 
+    -- Subconsulta para obtener los paquetes
     (SELECT
         esh.[EXPANDED_PACKAGE_TRACKING_NUMBER] AS trackingNumber,
         esh.[PACKAGE_WEIGHT] AS weight,
         esh.[PACKAGE_WEIGHT_UNIT] AS weightUnit,
         esh.[OVERSIZE_PACKAGE_FLAG] AS oversize
      FROM [dbo].[EXPORT_SHIPMENT_HIJOS] esh
-     WHERE esh.[CONSECUTIVO_SHIPMENT] = es.[CONSECUTIVO]  -- Filtrar por el mismo consecutivo
+     WHERE esh.[CONSECUTIVO_SHIPMENT] = es.[CONSECUTIVO]
      FOR JSON PATH) AS package,
 
+    -- Subconsulta para obtener información del primer paquete
     (SELECT
         es.[WEIGHT_1_PACKAGE_IN_SHIPMENT] AS weight1Package,
         es.[WEIGHT_UNIT_1_PACKAGE] AS weightUnit1Package,
@@ -80,10 +85,11 @@ const query =
         es.[OVERSIZE_PACKAGE_FLAGS] AS oversizeFlags
      FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS packagePush,
 
+    -- Subconsulta para información del remitente (shipper)
     (SELECT
         es.[SHIPPER_NUMBER] as number,
         es.[SHIPPER_COMPANY] AS company,
-        (ISNULL(es.[SHIPPER_BUILDING], '') + ' ' + ISNULL(es.[SHIPPER_STREET], '')) AS address ,
+        (ISNULL(es.[SHIPPER_BUILDING], '') + ' ' + ISNULL(es.[SHIPPER_STREET], '')) AS address,
         es.[SHIPPER_CITY] AS city,
         es.[SHIPPER_COUNTY] AS county,
         es.[SHIPPER_STATE] AS state,
@@ -94,36 +100,34 @@ const query =
         es.[SHIPPER_CUSTOMS_EIN_NUMBER] as customEINNumber
      FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS shipper,
 
+    -- Subconsulta para información del consignatario (consignee)
     (SELECT
         es.[CONSIGNEE_COMPANY] AS company,
         es.[CONSIGNEE_CONTACT_NAME] AS contactName,
-        (ISNULL(es.[CONSIGNEE_BUILDING], '') + ' ' + ISNULL(es.[CONSIGNEE_STREET], '')) AS address ,
+        (ISNULL(es.[CONSIGNEE_BUILDING], '') + ' ' + ISNULL(es.[CONSIGNEE_STREET], '')) AS address,
         es.[CONSIGNEE_CITY] AS city,
         es.[CONSIGNEE_COUNTY] AS county,
         es.[CONSIGNEE_STATE] AS state,
         es.[CONSIGNEE_POSTAL_CODE] AS postalCode,
         es.[CONSIGNEE_COUNTRY_CODE] AS country,
         es.[CONSIGNEE_PHONE_NUMBER] AS phoneNumber,
-            es.[CONSIGNEE_FAX_NUMBER] as faxNumber ,
-          es.[CONSIGNEE_PO_NUMBER] as poNumber,
-    es.[ALTERNATE_TRACKING_NUMBER_2] as alternateTrackingNumber2
+        es.[CONSIGNEE_FAX_NUMBER] as faxNumber,
+        es.[CONSIGNEE_PO_NUMBER] as poNumber,
+        es.[ALTERNATE_TRACKING_NUMBER_2] as alternateTrackingNumber2
      FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS consignee,
 
+    -- Subconsulta para obtener información de terceros
     (SELECT
         es.[SHIPPER_3RD_PARTY_ACCOUNT_NUMBER] AS accountNumber,
         es.[SHIPPER_3RD_PARTY_COMPANY] AS company,
         es.[SHIPPER_3RD_PARTY_COUNTRY] AS country
      FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS shipper3Party
 
-    FROM [dbo].[EXPORT_SHIPMENT] es
-    LEFT JOIN [dbo].[EXPORT_SHIPMENT_INVOICE] esi
-        ON es.[CONSECUTIVO] = esi.[CONSECUTIVO_SHIPMENT]
-    LEFT JOIN [dbo].[EXPORT_SHIPMENT_HIJOS] esh
-        ON es.[CONSECUTIVO] = esh.[CONSECUTIVO_SHIPMENT]
-
-     WHERE FECHA_CARGA >= '2020-01-01'
-  AND FECHA_CARGA <= '2020-02-25'
-    ORDER BY es.[FECHA_CARGA] DESC`
+FROM [dbo].[EXPORT_SHIPMENT] es
+WHERE es.[FECHA_CARGA] >= '2022-01-01'
+  AND es.[FECHA_CARGA] <= '2024-10-08'
+  AND es.[SHIPMENT_ID] IS NOT NULL AND es.[SHIPMENT_ID] != ''
+ORDER BY es.[FECHA_CARGA] DESC;`
 
 const inicio = new Date()
 
@@ -146,7 +150,7 @@ function transform(data) {
   // Verifica si el resultado es un array
   if (Array.isArray(invoiceArray) && invoiceArray.length > 0) {
     // Convierte el array en un objeto usando solo la primera posición
-    data.invoice = invoiceArray[0]
+    data.invoice = invoiceArray
   }
 
 
